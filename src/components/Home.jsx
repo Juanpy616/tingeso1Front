e00e55f835daac77108c18aa83fba655e57ac1cf
Juanpy616 <juan.loyola@usach.cl>
2025-04-28 20:43:01 -0400

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
       <div>
         <h1>Bienvenid@ a KartingRM</h1>
         <p>
+            Hola mundo.
             Aplicación web para le gestión de KartingRM. Haga click en el menú arriba a la izquierda para navegar por la aplicación.
         </p>
       </div>
